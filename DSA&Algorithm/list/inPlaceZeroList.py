3b2dace0fcbf2c357525162d3e56061c9346178e
CodeWithRahul1 <rahulyadavbui64@gmail.com>
2025-04-11 15:30:06 +0530

    non zero list

--- DSA&Algorithm/list/inPlaceZeroList.py
+++ DSA&Algorithm/list/inPlaceZeroList.py
@@ -19,29 +19,16 @@ print(nums)
 ### second approch
 
 
-def move_zeros_and_sort(nums):
-    # Step 1: Count non-zero elements and collect them
-    non_zeros = []
+def move_zeros_in_place(nums):
     for i in range(len(nums)):
-        if nums[i] != 0:
-            non_zeros.append(nums[i])
-
-    # Step 2: Sort non-zero elements using simple bubble sort
-    for i in range(len(non_zeros)):
-        for j in range(i + 1, len(non_zeros)):
-            if non_zeros[i] > non_zeros[j]:
-                non_zeros[i], non_zeros[j] = non_zeros[j], non_zeros[i]
-
-    # Step 3: Put sorted non-zeros back and fill remaining with zeros
-    for i in range(len(non_zeros)):
-        nums[i] = non_zeros[i]
-    
-    for i in range(len(non_zeros), len(nums)):
-        nums[i] = 0
-
+        if nums[i]==0:
+            nums.pop(i)
+            nums.append(0)
+            
     return nums
+            
 
-# Example
-nums = [1, 0, 5, 0, 3, 4]
-move_zeros_and_sort(nums)
-print(nums)  # Output: [1, 3, 4, 5, 0, 0]
+
+nums = [0,1, 0, 5, 0, 3, 4, 6, 0,7,0,]
+move_zeros_in_place(nums)
+print(nums)
