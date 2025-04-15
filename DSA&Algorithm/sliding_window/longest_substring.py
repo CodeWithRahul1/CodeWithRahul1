ca6397ed3ae184754f2080b860d82f586fa7a095
CodeWithRahul1 <rahulyadavbui64@gmail.com>
2025-04-15 20:43:24 +0530

    latest

--- DSA&Algorithm/sliding_window/longest_substring.py
+++ DSA&Algorithm/sliding_window/longest_substring.py
@@ -15,3 +15,26 @@ def length_of_longest_substring(s: str) -> int:
 
 s = "abcabcbb"
 print(length_of_longest_substring(s))
+
+def longest_substring(s: str):
+    char_set = set()
+    left = 0
+    max_len = 0
+    start_idx = 0
+
+    for right in range(len(s)):
+        while s[right] in char_set:
+            char_set.remove(s[left])
+            left += 1
+        char_set.add(s[right])
+
+        if right - left + 1 > max_len:
+            max_len = right - left + 1
+            start_idx = left
+
+    return s[start_idx:start_idx + max_len]
+
+
+# Example usage
+s = "abcabcbb"
+print(longest_substring(s))  # Output: "abc"
