a1222fdc871f14742199e9dd8d5c38d7c28ce857
CodeWithRahul1 <rahulyadavbui64@gmail.com>
2025-04-15 13:58:17 +0530

    duck typing

--- DSA&Algorithm/basic_Python/txt.py
+++ DSA&Algorithm/basic_Python/txt.py
@@ -2,3 +2,36 @@ Encapsulation (Data Hiding)
 Polymorphism (Many Forms)
 Duck Typing
 mixin
+
+
+'''Duck Typing'''
+
+'''
+✅ Duck Typing Definition (for Interview)
+Duck typing is a programming concept used in dynamically typed languages like Python, where the type or class of an object is less important than the methods and properties that the object defines. In other words, an object’s suitability for use is determined by whether it "behaves like" the expected type, rather than whether it is that type.
+
+This concept comes from the phrase:
+
+“If it looks like a duck, swims like a duck, and quacks like a duck — then it probably is a duck.”
+
+In Python, this allows functions and classes to operate on any object that implements the required behavior (methods/attributes), enabling flexible and polymorphic code without requiring inheritance or explicit interfaces.'''
+
+#example
+
+class Duck:
+    def quack(self):
+        print("Quack!")
+
+class Person:
+    def quack(self):
+        print("I'm pretending to be a duck!")
+
+def make_it_quack(thing):
+    # Duck typing in action
+    thing.quack()
+
+duck = Duck()
+person = Person()
+
+make_it_quack(duck)   # Output: Quack!
+make_it_quack(person) # Output: I'm pretending to be a duck!
--- DSA&Algorithm/test.py
+++ DSA&Algorithm/test.py
@@ -1,47 +1,14 @@
-class Node:
+from onnx2tf import convert
 
-    def __init__(self, data):
-        self.data = data
-        self.next = None
+# Define the paths
+onnx_model_path = "/home/admin1/Downloads/best.onnx"
+output_tf_path = "/home/admin1/Downloads/best_tf_model"  # This will be a folder
 
+# Run the conversion
+convert(
+    input_onnx_file_path=onnx_model_path,
+    output_folder_path=output_tf_path,
+    copy_onnx_input_output_names_to_tflite=True  # Optional: useful if you'll convert to TFLite later
+)
 
-class LinkedList:
-
-    def __init__(self):
-
-        self.head = None
-
-
-    def InsertFromBegning(self, value):
-        
-        new_node = Node(value)
-        new_node.next = self.head
-        self.head = new_node
-
-    
-    def PrintLinst(self):
-        if self.head is None:
-            return 'List is Empty'
-
-        current_node = self.head
-        res = ''
-
-        while current_node is not None:
-
-            res += str(current_node.data) + '-->'
-            current_node = current_node.next 
-        return res
-    
-
-
-obj = LinkedList()
-obj.InsertFromBegning(3)
-obj.InsertFromBegning(4)
-obj.InsertFromBegning(5)
-obj.InsertFromBegning(6)
-print(obj.PrintLinst())
-
-
-
-
-        
+print(f"Model converted and saved to: {output_tf_path}")
